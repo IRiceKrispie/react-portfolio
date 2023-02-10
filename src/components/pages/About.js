c758bcd6ae1093b1d442b1ad538bd0fec41c4df1
IRiceKrispie <joseph.sandoval.cs6@gmail.com>
2023-02-10 17:34:08 -0600

    Added image to about page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import dog from '../images/dog.jpg';
+const styles = {
+    imgStyle: {
+      display: 'flex',
+      height: '200px',
+      width: '200px',
+    },
+};
 
 export default function About() {
   return (
     <div>
       <h1>About Page</h1>
+      <img style={styles.imgStyle} src={dog} alt={"German Shepherd"}></img>
       <p>
         Some of my qualifications include a coding bootcamp provided by the University of Texas at Austin and a certificate from JP Morgan Chase for completing their virtual training.
       </p>

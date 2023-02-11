56baa0108234e7975f4519c6f6575df042336f96
IRiceKrispie <joseph.sandoval.cs6@gmail.com>
2023-02-11 05:31:54 -0600

    added icons to footer with links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import gitHub from './images/github.256x252.png';
+import linkedin from './images/linkedin.png';
 
 const styles = {
     footerStyle: {
@@ -9,13 +11,22 @@ const styles = {
         width: '100%',
         background: '#999',
         fontSize: '30px',
-        textAlign: 'center',
+        display: 'flex',
+        justifyContent: 'center',
+        gap: '10px',
+    },
+    imgStyle: {
+        height: '40px',
+        with: '60px',
     },
 };
 
 function Footer() {
     return(
-    <footer style={styles.footerStyle}>Work in progress portfolio using the React framework</footer>
+    <footer style={styles.footerStyle}>
+        <div><a  href="https://github.com/IRiceKrispie" target="_blank" rel="noreferrer"><img style={styles.imgStyle} src={gitHub} alt="gitHub logo"></img></a></div>
+        <div><a  href="https://www.linkedin.com/in/joseph-sandoval-70b177259/" target="_blank" rel="noreferrer"><img style={styles.imgStyle} src={linkedin} alt="linkedIn logo"></img></a></div>
+    </footer>
     );
     
 }
